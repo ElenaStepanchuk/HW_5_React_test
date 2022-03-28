c6ed4ddcfdd6ea26eef1ef99a5cc02ac9c575f0c
ElenaStepanchuk <podolyak365@gmail.com>
2022-03-28 12:42:54 +0300

    MovieDetailsPage

--- src/components/MovieDetailsPage.js
+++ src/components/MovieDetailsPage.js
@@ -1,31 +1,59 @@
 import { Link, useParams } from 'react-router-dom';
+import { nanoid } from 'nanoid';
 import { DetailsFilm } from '../helpers/FetchFilms';
 import { useEffect, useState } from 'react';
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
-  const [details, setDetails] = useState(null);
+  const [detail, setDetail] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const Details = async () => {
+      setLoading(true);
       try {
         const response = await DetailsFilm(movieId);
         console.log(response.data);
-        setDetails(response);
+        setDetail(response.data);
       } catch (error) {
         console.log(error);
       } finally {
-        console.log(details);
+        setLoading(false);
       }
     };
     Details();
   }, [movieId]);
+  // console.log(detail.id);
+  const handleBackBtn = () => {
+    setDetail(null);
+  };
 
   return (
     <div>
-      <h2>{movieId}</h2>
+      {detail && <h2>{detail.original_title || detail.name}</h2>}
+      <h3>Release date:</h3>
+      {detail && <p>{detail.release_date}</p>}
+      {detail && (
+        <img
+          src={`https://image.tmdb.org/t/p/w500${detail.poster_path}`}
+          alt={detail.original_title || detail.name}
+        />
+      )}
+      <h3>Genres: </h3>
+      {detail &&
+        detail.genres.map(genre => {
+          const id = nanoid();
+          return <p key={id}>{genre.name}</p>;
+        })}
+      <h3>Descriptions: </h3>
+      {detail && <p>{detail.overview}</p>}
+      <h3>Homepage: </h3>
+      {detail && <a href={`${detail.homepage}`}> {detail.homepage}</a>}
+      <br />
       <Link to="/">
-        <button>Go back</button>
+        <button type="button" onChange={handleBackBtn}>
+          Go back
+        </button>
       </Link>
     </div>
   );
